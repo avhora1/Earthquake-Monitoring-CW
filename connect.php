4662d29c8786899d298a6ae2b291e07867ea3905
DYSON\zbeck <zachary.beck@dyson.com>
2025-03-10 13:49:43 +0000

    Added connect.php file that allows us to connec to database server and added the data base breakdown pdf so we know what the databases should look like

---
+++ connect.php
@@ -0,0 +1,24 @@
+<?php
+$serverName = "UK-DIET-SQL-T1"; //serverName\instanceName
+
+//Database name = Enter username (UID) and password (PWD) for your group
+$connectionInfo = array( "Database"=>"Group6_DB", "UID"=>"UserGroup6", "PWD"=>"8L9GtfJA8L9GtfJA");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Connection established to database.<br />"; 
+	 $server_info = sqlsrv_server_info($conn);
+	 if( $server_info )
+		{			
+			/**foreach( $server_info as $key => $value) {
+			   echo $key.": ".$value."<br />";
+			}**/
+		} else {
+			  die( print_r( sqlsrv_errors(), true));
+		}																			
+	 
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
